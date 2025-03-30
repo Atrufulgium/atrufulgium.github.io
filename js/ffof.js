91996d5ebb1ebb6ae86c44c2df878c5ed5098aaa
Atrufulgium <atrufulgium@gmail.com>
2025-03-31 01:08:07 +0200

    Finalized FFoF assets

--- js/ffof.js
+++ js/ffof.js
@@ -93,6 +93,9 @@ function step() {
     // 3 fps. The 1600 is the lcm of 160px width, and the three different speeds 5x, 2x, 1x.
     let bg_anim_offset = Math.floor(3 * progress_seconds) % 1600;
     document.body.style.setProperty("--anim-offset", bg_anim_offset.toString());
+    // 4 fps for the details in the animation, just like in aseprite
+    let detail_anim_offset = Math.floor(4 * progress_seconds) % 4;
+    document.body.style.setProperty("--detail-anim-offset", detail_anim_offset.toString());
     // After 22 frames of offset, we're at an empty frame, so don't move past that.
     let azzy_offset = Math.min(Math.floor(10 * progress_seconds), 22);
     document.body.style.setProperty("--azachon-anim-offset", azzy_offset.toString());
